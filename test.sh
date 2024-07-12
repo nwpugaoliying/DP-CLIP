d1c28dc5440a9fa76bfb6d46e525edb22cabffde
nwpugaoliying <70734068+nwpugaoliying@users.noreply.github.com>
2024-07-12 13:26:24 +0800

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
-python -m exp.train --prompt=CS_deep --exp_name=DP-CLIP --lora=True --lora_scale=True --local=4 --mode=test
+python -m exp.train --prompt=CS_deep --exp_name=DP-CLIP --lora=True --lora_scale=True --local=4 \
+--mode=test --model_name='DP-CLIP_trained_model.ckpt'
 
